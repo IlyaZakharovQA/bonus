b50819274e9c4589a4a9db75ee209fff9b2a4326
Ilya Zakharov QAMID-50 <oiska@bk.ru>
2022-11-18 22:44:51 +0300

    Reformat code

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,14 @@ public class Main {
         int account = 50; // изначально на счету
         int refill = 2000; // сумма пополнения
         int bonus;
-        if (refill >=1000) {
+        if (refill >= 1000) {
             bonus = refill / 100;
         } else {
             bonus = 0;
         }
         int finalAccount = account + refill + bonus; // итоговый счёт
         if (bonus == 0) {
-            System.out.println("Бонусов нет, итоговая сумма на счету клиента - " + finalAccount+ "₽.");
+            System.out.println("Бонусов нет, итоговая сумма на счету клиента - " + finalAccount + "₽.");
         } else {
             System.out.println("Клиент пополнил счёт на " + refill + "₽. Бонус равен " + bonus + "₽, итоговая сумма на счету клиента - " + finalAccount + "₽.");
         }
